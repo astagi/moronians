72e4a411895735de70022cbabc50b67469ccd41b
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-07-15 14:14:12 -0400

    Add more words to the spanish -> english module, add UTF support

--- modules/languages/spanish_english/level_1/generators.py
+++ modules/languages/spanish_english/level_1/generators.py
@@ -1,17 +1,32 @@
+# -*- coding: utf-8 -*-
 from random import choice
 
 
 word_list_spanish_english = [
-    ('avion', 'airplane'),
-    ('rojo', 'red'),
-    ('azul', 'blue'),
-    ('ayer', 'yesterday'),
-    ('jugar', 'play'),
-    ('foto', 'photo'),
-    ('padre', 'father'),
-    ('madre', 'mother'),
-    ('abuelo', 'grandfather'),
-    ('abuela', 'grandmother'),
+    (u'avión', 'airplane'),
+    (u'rojo', 'red'),
+    (u'azul', 'blue'),
+    (u'ayer', 'yesterday'),
+    (u'jugar', 'play'),
+    (u'foto', 'photo'),
+    (u'padre', 'father'),
+    (u'madre', 'mother'),
+    (u'hoy', 'today'),
+    (u'ahora', 'now'),
+    (u'alto', 'tall'),
+    (u'corto', 'short'),
+    (u'largo', 'long'),
+    (u'perro', 'dog'),
+    (u'gato', 'cat'),
+    (u'carro', 'carr'),
+    (u'año', 'year'),
+    (u'casa', 'house'),
+    (u'aire', 'air'),
+    (u'cielo', 'sky'),
+    (u'nube', 'cloud'),
+    (u'bote', 'boat'),
+    (u'mar', 'sea'),
+    (u'oceano', 'ocean'),
 ]
 
 
